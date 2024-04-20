aee361b583bed22b07269ab1648cfd8ed1c537f1
Luan-art <luanlf2003@gmail.com>
2024-04-19 21:53:31 -0300

    Add project files.

---
+++ aumentoSalarial/Program.cs
@@ -0,0 +1,18 @@
+﻿float salarioAntigo = 0, salarioNovo = 0, porcentagemSalario = 0;
+
+do
+{
+    Console.WriteLine("Digite o seu antigo salário: ");
+    salarioAntigo = float.Parse(Console.ReadLine());
+
+} while (salarioAntigo <= 0);
+
+do
+{
+    Console.WriteLine("Digite o salario com aumento: ");
+    salarioNovo = float.Parse(Console.ReadLine());
+
+} while (salarioNovo <= salarioAntigo);
+
+porcentagemSalario = ((salarioNovo - salarioAntigo) / salarioAntigo) * 100;
+Console.WriteLine($"O porcentual de aumento é {porcentagemSalario} %");
